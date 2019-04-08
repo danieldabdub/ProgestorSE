4f8a03016383ceae35a0bf74fccb45e252bd2cc8
Alejandro <alejandro@notreply.plaza.com>
2019-04-08 16:15:06 +0200

    Update menu.java

--- WEB-INF/classes/menu.java
+++ WEB-INF/classes/menu.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 import java.sql.Connection;
 
 @SuppressWarnings("serial")
-public class ProductList extends HttpServlet {
+public class menu extends HttpServlet {
     Connection connection;
 
     public void init(ServletConfig config) throws ServletException {
@@ -22,23 +22,23 @@ public class ProductList extends HttpServlet {
         
         toClient.println(Utils.header())
         
-        toClient.println('<body>');
-        toClient.println('<div class="navbar-inner">');
-        toClient.println('<div class="container">');
-        toClient.println('<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">');
-        toClient.println('<span class="icon-bar"></span>');
-        toClient.println('<span class="icon-bar"></span>');
-        toClient.println('<span class="icon-bar"></span>');
-        toClient.println('<A HREF="Menu.html" >');
-        toClient.println('<IMG SRC="logoMenu.png" style="float:left;width:798px;height:450px;"></A>');
-        toClient.println('<div class="nav-collapse collapse pull-right">');
-        toClient.println('<ul class="nav"></ul></div></div></div>');
-        toClient.println('<div class="main">');
-        toClient.println('<br><br><br><br>');
-        toClient.println('<h1><font size = "30">Welcome '+ managerId +'</font></h1></div>');
-        toClient.println('');
-        toClient.println('');
-        toClient.println('');
+        toClient.println("<body>");
+        toClient.println("<div class='navbar-inner'>");
+        toClient.println("<div class='container'>");
+        toClient.println("<a class='btn btn-navbar' data-toggle='collapse' data-target='.nav-collapse'>");
+        toClient.println("<span class='icon-bar'></span>");
+        toClient.println("<span class='icon-bar'></span>");
+        toClient.println("<span class='icon-bar'></span>");
+        toClient.println("<A HREF='Menu.html' >");
+        toClient.println("<IMG SRC='logoMenu.png' style='float:left;width:798px;height:450px;'></A>");
+        toClient.println("<div class='nav-collapse collapse pull-right'>");
+        toClient.println("<ul class='nav'></ul></div></div></div>");
+        toClient.println("<div class='main'>");
+        toClient.println("<br><br><br><br>");
+        toClient.println("<h1><font size = '30'>Welcome "+ managerId +"</font></h1></div>");
+        toClient.println("");
+        toClient.println("");
+        toClient.println("");
         
         toClient.println(Utils.footer())
     }
